f6666e45cf9293d8308ab7f703ec88b06586e0a4
Leonardo Klinger <60077995+leonardoklinger@users.noreply.github.com>
2021-10-05 13:28:26 -0300

    Delete start.js

--- FrontEnd/Eventos/start.js
+++
@@ -1,6 +0,0 @@
-const Discord = require('discord.js');
-exports.run = async (client) => {
-client.on("ready", ready => {//evento quando o bot liga !
-        console.log(`Bot foi iniciado, com ${client.users.cache.size} usuários, em ${client.channels.cache.size} canais, em ${client.guilds.cache.size} servidores.`);
-    })
-}
